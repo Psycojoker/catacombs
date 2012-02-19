cd08c7a47b24617fc4ddefe572891268d65b7bdc
Laurent Peuch <cortex@worlddomination.be>
2012-02-19 23:56:34 +0100

    [mod] don't show path of the book in the public data

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ def _format_book(book):
     book["_id"] = str(book["_id"])
     book["info_url"] = "/info/%s" % book["_id"]
     book["get_url"] = "/get/%s" % book["_id"]
+    del book["path"]
     return book
 
 def connect():
