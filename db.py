1d825efcdd702296b2a3f47d9ad9f8d21d726418
Laurent Peuch <cortex@worlddomination.be>
2012-02-19 22:44:37 +0100

    [mod] refactoring: add a _format_book method to prepare bson before returning it

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import pymongo
 
+def _format_book(book):
+    book["_id"] = str(book["_id"])
+    book["info_url"] = "/info/%s" % book["_id"]
+    book["get_url"] = "/get/%s" % book["_id"]
+    return book
+
 def connect():
     return pymongo.Connection()["catacombs"]
 
 def get_books():
-    a = list(connect()['books'].find())
-    for i in a:
-        i["_id"] = str(i["_id"])
-        i["info_url"] = "/info/%s" % i["_id"]
-        i["get_url"] = "/get/%s" % i["_id"]
-    return a
+    return map(_format_book, list(connect()['books'].find()))
