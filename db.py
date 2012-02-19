32e12f579e5ee3abda0e2508fe90969fac017eb9
Laurent Peuch <cortex@worlddomination.be>
2012-02-19 20:15:05 +0100

    [enh] make the api self describing

--- db.py
+++ db.py
@@ -7,4 +7,6 @@ def get_books():
     a = list(connect()['books'].find())
     for i in a:
         i["_id"] = str(i["_id"])
+        i["info_url"] = "/info/%s" % i["_id"]
+        i["get_url"] = "/get/%s" % i["_id"]
     return a
