5c34d229937529d0d6824167c30c777dfdbe9f33
Laurent Peuch <cortex@worlddomination.be>
2012-02-19 23:02:22 +0100

    [enh] code the about

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@
 import selector
 from json import dumps
 import db
+import os
 
 extensions = {
     'html': 'text/html',
@@ -25,7 +26,12 @@ def info_on_a_book(environ, start_response):
     id = environ['selector.vars']['id']
     return render(start_response, dumps(db.get_a_book(id)))
 
+def about(environ, start_response):
+    print start_response
+    return render(start_response, dumps({"owner": os.environ["USER"]}))
+
 urls = selector.Selector()
 urls.add('/', GET=home)
 urls.add('/books', GET=books)
 urls.add('/info/{id}', GET=info_on_a_book)
+urls.add('/about', GET=about)
